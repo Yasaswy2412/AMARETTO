0cea90abfb209da28f2ba5c2ab91c311ca8b4fc2
Yasaswy2412 <50540059+Yasaswy2412@users.noreply.github.com>
2021-03-09 12:20:54 +0530

    Update amaretto_initialize.R

--- R/amaretto_initialize.R
+++ R/amaretto_initialize.R
@@ -4,6 +4,7 @@
 #' @return result
 #' @keywords internal
 CreateRegulatorData <- function(MA_matrix = MA_matrix, CNV_matrix = NULL, MET_matrix = NULL, Driver_list = NULL, PvalueThreshold = 0.001, RsquareThreshold = 0.1, method = "union") {
+    cat("\t Display from Yasaswy2412 Repository")
     if (is.null(Driver_list)) 
         DriversList <- NULL
     if (is.null(CNV_matrix)) 
